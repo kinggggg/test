3940b316fd3df029f659d3e4511d46e3dfb88b21
zeeeek <liweibooooo@126.com>
2017-03-08 20:36:41 +0800

    添加quartz测试用例

---
+++ src/test/java/com/zeek/morphia/entity/test/quartz/DumbJob.java
@@ -0,0 +1,25 @@
+package com.zeek.morphia.entity.test.quartz;
+
+import org.quartz.*;
+
+/**
+ * Created by weibo_li on 2017/3/8.
+ */
+public class DumbJob implements Job {
+
+    public DumbJob() {
+    }
+
+    public void execute(JobExecutionContext context)
+            throws JobExecutionException
+    {
+        JobKey key = context.getJobDetail().getKey();
+
+        JobDataMap dataMap = context.getJobDetail().getJobDataMap();
+
+        String jobSays = dataMap.getString("jobSays");
+        float myFloatValue = dataMap.getFloat("myFloatValue");
+
+        System.err.println("Instance " + key + " of DumbJob says: " + jobSays + ", and val is: " + myFloatValue);
+    }
+}
