af2f18394130ad3d1a6f0e460f2a3e6d9890dd34
zeeeek <liweibooooo@126.com>
2017-04-10 15:35:33 +0800

    增加Map测试

---
+++ src/test/java/com/zeek/test/map/MapTest.java
@@ -0,0 +1,33 @@
+package com.zeek.test.map;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by weibo_li on 2017/4/10.
+ */
+public class MapTest {
+
+    @Test
+    public void test() {
+        Map<String, Map<String, String>> certificationOrganizationCodeMap = new HashMap<>();
+
+        Map<String, String> qiyeJobCodeMap = new HashMap<>();
+        qiyeJobCodeMap.put("0101", "法务");
+        qiyeJobCodeMap.put("0102", "公司律师");
+        qiyeJobCodeMap.put("0103", "高管");
+        qiyeJobCodeMap.put("0104", "工作人员");
+        certificationOrganizationCodeMap.put("01", qiyeJobCodeMap);
+
+        //法院
+        qiyeJobCodeMap = new HashMap<>();
+        qiyeJobCodeMap.put("0201", "法官");
+        qiyeJobCodeMap.put("0202", "书记员");
+        qiyeJobCodeMap.put("0203", "工作人员");
+        certificationOrganizationCodeMap.put("02", qiyeJobCodeMap);
+
+
+    }
+}
