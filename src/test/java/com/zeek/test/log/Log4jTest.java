4f5580bc103a31eacc19822e79a8512dfb0192ef
zeeeek <liweibooooo@126.com>
2017-04-10 17:27:00 +0800

    练习log4j

---
+++ src/test/java/com/zeek/test/log/Log4jTest.java
@@ -0,0 +1,18 @@
+package com.zeek.test.log;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by weibo_li on 2017/4/1.
+ */
+public class Log4jTest {
+
+    Logger logger = LoggerFactory.getLogger(Log4jTest.class);
+
+    @Test
+    public void test() {
+        logger.info("info");
+    }
+}
