a6bd9e1cb890f3a58d13253f571370cf677c8927
zeeeek <liweibooooo@126.com>
2017-04-27 14:21:48 +0800

    ArrayList subList方法测试

--- src/test/java/com/zeek/test/arraylist/ArrayListTest.java
+++ src/test/java/com/zeek/test/arraylist/ArrayListTest.java
@@ -1,15 +1,67 @@
 package com.zeek.test.arraylist;
 
+import com.zeek.test.domain.User;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by weibo_li on 2017/4/25.
  */
 public class ArrayListTest {
+
+    @Test
+    public void test3() {
+        List<User> usersList = new ArrayList<>();
+        User one = new User("1", "张三");
+        usersList.add(one) ;
+        usersList.add(one) ;
+        usersList.add(one) ;
+        usersList.add(new User("2", "lisi")) ;
+
+        usersList.stream().forEach(user -> {
+            System.out.println(user.toString());
+        });
+
+        System.out.println("=======================");
+
+        Set<User> usersSet = new HashSet<>();
+        usersSet.addAll(usersList);
+        usersList.clear();
+        usersList.addAll(usersSet);
+
+        usersList.stream().forEach(user -> {
+            System.out.println(user.toString());
+        });
+
+
+
+
+    }
+
+    @Test
+    public void test2() {
+        List<User> users = new ArrayList<>();
+        User one = new User("1", "zhangsan");
+        users.add(one) ;
+        users.add(new User("2", "lisi")) ;
+
+        List<User> users1 = users.subList(0, 1);
+        users1.stream().forEach(user -> {
+            user.setId("3");
+            System.out.println(user.toString());
+        });
+
+        users1.clear();
+
+        System.out.println("=======================");
+
+        users.stream().forEach(user -> {
+            System.out.println(user.toString());
+        });
+
+
+    }
 
     @Test
     public void test1() {
