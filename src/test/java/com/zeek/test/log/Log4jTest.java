fb1de4bc09561a2cfab7a827a29fdf333781f264
zeeeek <liweibooooo@126.com>
2017-06-19 10:27:25 +0800

    log4j test code commit

--- src/test/java/com/zeek/test/log/Log4jTest.java
+++ src/test/java/com/zeek/test/log/Log4jTest.java
@@ -3,6 +3,7 @@ package com.zeek.test.log;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.junit.Test;
 
 import java.util.Date;
@@ -12,9 +13,20 @@ import java.util.Date;
  */
 public class Log4jTest {
 
-
     @Test
     public void test2() {
+
+        PropertyConfigurator.configure(this.getClass().getClassLoader()
+                .getResourceAsStream("log4j.properties"));
+
+        Logger log=Logger.getLogger("cd.itcast.log");
+        log.debug("log debug..");
+        log.info("log info..");
+
+    }
+
+    @Test
+    public void test1() {
         BasicConfigurator.configure();
 
         Logger logger = Logger.getLogger("cd.itcast");
