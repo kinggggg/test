d64f27da1cdc0517e917e0f8482de55b38b2a403
zeeeek <liweibooooo@126.com>
2017-06-26 16:57:12 +0800

    log test

--- src/test/java/com/zeek/test/log/Log4jTest2.java
+++ src/test/java/com/zeek/test/log/Log4jTest2.java
@@ -1,11 +1,21 @@
 package com.zeek.test.log;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Created by weibo_li on 2017/4/1.
  */
 public class Log4jTest2 {
+
+    @Test
+    public void test1() {
+
+        DOMConfigurator.configure("log4j.xml");
+
+        Logger log = Logger.getLogger("cd.itcast.log");
+        log.debug("log debug..");
+        log.info("log info..");
+    }
 }
