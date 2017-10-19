b0dea9d4fcd265a5cfb57640c3e10102d5c85f48
zeeeek <liweibooooo@126.com>
2017-10-19 08:32:23 +0800

    私有构造方法不能在类外实例化对象；但是可以在类当中直接new对象

---
+++ src/test/java/com/zeek/test/construct/ConstructTest.java
@@ -0,0 +1,12 @@
+package com.zeek.test.construct;
+
+import org.junit.Test;
+
+public class ConstructTest {
+
+    @Test
+    public void test1() {
+
+
+    }
+}
---
+++ src/test/java/com/zeek/test/construct/Dog.java
@@ -0,0 +1,12 @@
+package com.zeek.test.construct;
+
+public class Dog {
+
+    private Dog() {
+
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+    }
+}
