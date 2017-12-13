2a4658d5b225bb47129382b5a64f4b5ba282b2ca
zeeeek <liweibooooo@126.com>
2017-12-13 15:18:25 +0800

    CharBuffer测试

---
+++ src/test/java/com/zeek/test/ByteBuffer/ByteBufferTest.java
@@ -0,0 +1,27 @@
+package com.zeek.test.ByteBuffer;
+
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+
+public class ByteBufferTest {
+
+    @Test
+    public void test0() {
+
+        String str = "helloWorld";
+        ByteBuffer buff  = ByteBuffer.wrap(str.getBytes());
+        System.out.println("position:"+buff.position()+"\t limit:"+buff.limit());
+        //读取两个字节
+        byte b = buff.get();
+        System.out.println(new String(new byte[]{b}));
+        byte bb = buff.get();
+        System.out.println(new String(new byte[]{bb}));
+        System.out.println("position:"+ buff.position()+"\t limit:"+buff.limit());
+        buff.mark();
+        System.out.println("position:"+buff.position()+"\t limit:"+buff.limit());
+        buff.flip();
+        System.out.println("position:"+buff.position()+"\t limit:"+buff.limit());
+
+    }
+}
