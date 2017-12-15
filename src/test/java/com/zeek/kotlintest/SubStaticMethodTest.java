d7db3a79a3017a247dd8a822902af317a82bd7d6
zeeeek <liweibooooo@126.com>
2017-12-15 10:07:48 +0800

    父类静态方法不能被子类重写

---
+++ src/test/java/com/zeek/kotlintest/SubStaticMethodTest.java
@@ -0,0 +1,13 @@
+package com.zeek.kotlintest;
+
+/**
+ * @author: weibo_li
+ * @since: 2017-12-14 下午10:53
+ */
+public class SubStaticMethodTest extends StaticMethodTest {
+
+    @Override
+    public void test1() {
+        super.test1();
+    }
+}
