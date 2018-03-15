68d606b67985d8b2920d3f10c53c44af5fbe283b
zeeeek <liweibooooo@126.com>
2018-03-15 11:26:10 +0800

    commit

--- src/main/java/com/zeek/test/spring/learn/controller/TestController.java
+++ src/main/java/com/zeek/test/spring/learn/controller/TestController.java
@@ -3,6 +3,9 @@ package com.zeek.test.spring.learn.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 /**
  * @author weibo_li
@@ -11,8 +14,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/")
 public class TestController {
 
+    private final String CAS_CLIENT1_BASE_URL = "http://cas.client1.com:8888/" ;
+
+    @RequestMapping(value = "/jsonData", method = RequestMethod.GET)
+    public String jsonData() throws Exception {
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getForObject(CAS_CLIENT1_BASE_URL+"api/list", )
+
+        return null;
+
+    }
+
     @RequestMapping(value = "/loginPage", method = RequestMethod.GET)
-    public String redirect() throws Exception {
+    public String loginPage() throws Exception {
 
         return "login/loginPage";
 
