26291d46861d8caf0f1947c2609ec421a6975244
zeeeek <liweibooooo@126.com>
2018-03-15 10:14:56 +0800

    commit

--- src/main/java/com/zeek/test/spring/learn/controller/TestController.java
+++ src/main/java/com/zeek/test/spring/learn/controller/TestController.java
@@ -1,26 +1,11 @@
 package com.zeek.test.spring.learn.controller;
 
-import com.google.gson.Gson;
-import com.zeek.test.spring.learn.domain.User;
-import com.zeek.test.spring.learn.utils.CasLoginData;
 import com.zeek.test.spring.learn.utils.CasServerUtil;
-import com.zeek.test.spring.learn.utils.HttpClient;
-import com.zeek.test.spring.learn.utils.HttpMethod;
-import com.zeek.test.spring.learn.viewmodel.login.ExecutionData;
 import com.zeek.test.spring.learn.viewmodel.login.LoginData;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author weibo_li
@@ -29,121 +14,11 @@ import java.util.List;
 @RequestMapping(value = "/")
 public class TestController {
 
-    private final static String APP_URL = "http://cas.client1.com:8383/api/list";
-
-    @RequestMapping(value = "/api/list", method = RequestMethod.GET)
-    @ResponseBody
-    public Object apiList() {
-
-        User user1 = new User("xiaoming", 18);
-        User user2 = new User("xiaohong", 20);
-
-        List<User> users = new ArrayList<>();
-        users.add(user1);
-        users.add(user2);
-
-        return users;
-    }
-
-    @RequestMapping(value = "/execution", method = RequestMethod.GET)
-    @ResponseBody
-    public Object getExecution(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-
-        String service = httpServletRequest.getParameter("service");
-//        service = "http://cas.client1.com:8383/success" ;
-
-        String execution = CasServerUtil.getExecution(service);
-
-        ExecutionData executionData = new ExecutionData();
-        executionData.setExecution(execution);
-
-        return executionData;
-
-    }
-
     @RequestMapping(value = "/loginPage", method = RequestMethod.GET)
-    public String loginPage(Model model) throws Exception {
+    public String redirect() throws Exception {
 
-        String execution = CasServerUtil.getExecution(APP_URL);
-
-        model.addAttribute("execution", execution);
-
-        return "login/login" ;
+        return "login/loginPage";
 
     }
 
-    @RequestMapping(value = "/success", method = RequestMethod.GET)
-    public String loginSuccess(Model model) throws Exception {
-
-        String execution = CasServerUtil.getExecution(APP_URL);
-
-        model.addAttribute("execution", execution);
-
-        return "success/success" ;
-
-    }
-
-    @RequestMapping(value = "/api/loginSuccess", method = RequestMethod.GET)
-    public String loginApiSuccess(Model model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-        String new_url = "http://sso.frontend1.com:3000" ;
-
-        Cookie[] cookies = request.getCookies();
-
-        return "redirect:http://sso.frontend1.com:3000?cookie=" +  cookies[0].getValue();
-
-    }
-
-//    @RequestMapping(value = "/api/loginSuccess", method = RequestMethod.GET)
-//    public ModelAndView loginApiSuccess(Model model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-//
-//        String new_url = "http://sso.frontend1.com:3000" ;
-//
-//        Cookie[] cookies = request.getCookies();
-//        Cookie cookie = new Cookie(cookies[0].getName(), cookies[0].getValue());
-//        cookie.setMaxAge(3600);
-//        cookie.setDomain("sso.frontend1.com");
-//        response.addCookie(cookie);
-//
-//        String html = "<script type='text/javascript'>location.href='"+new_url+"';</script>";
-//        response.getWriter().print(html);
-//
-//        return null;
-//
-//    }
-
-//    @RequestMapping(value = "/api/loginSuccess", method = RequestMethod.GET)
-//    public String loginApiSuccess(Model model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-//
-//        return "redirect:http://sso.frontend1.com:3000";
-//
-//    }
-
-    @RequestMapping(value = "/redirect", method = RequestMethod.POST)
-    public String redirect(@RequestBody(required = false) LoginData loginData) throws Exception {
-
-        System.out.println(loginData);
-
-        String serviceTicket = CasServerUtil.validateFromCAS();
-
-        return "redirect:" + APP_URL + "?ticket=" + serviceTicket;
-
-    }
-
-    @RequestMapping(value = "/loginCookie", method = RequestMethod.GET)
-    public void loginCookie(@RequestBody(required = false) LoginData loginData, HttpServletResponse httpResponse) throws Exception {
-
-        System.out.println(loginData);
-
-        org.apache.http.cookie.Cookie casCookie = CasServerUtil.getCasServerCookieByHttpClientCall();
-
-        Cookie cookie = new Cookie(casCookie.getName(), casCookie.getValue());
-        cookie.setPath(casCookie.getPath());
-        cookie.setSecure(casCookie.isSecure());
-        cookie.setDomain("server.com");
-        cookie.setHttpOnly(true);
-
-        httpResponse.addCookie(cookie);
-
-    }
 }
---
+++ src/main/webapp/WEB-INF/views/login/loginPage.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: weibo_li
+  Date: 2018/3/15
+  Time: 上午10:09
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>登录页面1</title>
+</head>
+<body>
+<form action="http://sso.server.com:" method="GET">
+    站点名: <input type="text" name="name">
+    <br />
+    网址: <input type="text" name="url" />
+    <input type="submit" value="提交" />
+</form>
+</body>
+</html>
