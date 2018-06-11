beee682f5c9a2b8c096eb2060f6565456ed2a738
zeeeek <liweibooooo@126.com>
2018-06-11 13:36:10 +0800

    test

---
+++ src/test/java/com/zeek/jvm/Main.java
@@ -0,0 +1,18 @@
+package com.zeek.jvm;
+
+/**
+ * @author: weibo_li
+ * @since: 2018-06-05 上午9:59
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Integer i = 127;
+        Integer j = 127;
+
+        System.out.println(i == j);
+
+    }
+}
