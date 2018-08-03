5da36379e7c1a9554b0544130769dd66e345aaa0
zeeeek <liweibooooo@126.com>
2018-08-03 16:28:12 +0800

    自动生成代码代码初始版

---
+++ src/test/java/com/zeek/javatest/javacodegenerate/PropertyType.java
@@ -0,0 +1,12 @@
+package com.zeek.javatest.javacodegenerate;
+
+/**
+ * 属性类型枚举类
+ *
+ * @author: weibo_li
+ * @since: 2018-08-03 下午4:08
+ */
+public enum PropertyType {
+
+    Byte, Short, Int, Long, Boolean, Float, Double, String, ByteArray, Date
+}
