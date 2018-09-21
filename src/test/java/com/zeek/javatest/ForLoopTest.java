293d72ef39d5abde8d58a38d9a83398048bce086
zeeeek <liweibooooo@126.com>
2018-09-21 13:57:15 +0800

    for测试

--- src/test/java/com/zeek/javatest/ForLoopTest.java
+++ src/test/java/com/zeek/javatest/ForLoopTest.java
@@ -11,9 +11,18 @@ public class ForLoopTest {
     @Test
     public void forTest() {
         for(int count = 0; count < 10 ; count++) {
+//            if(count == 5) {
+//                break;
+//            }
+
             if(count == 5) {
-                break;
+                try {
+                    int i = 1 / 0;
+                }catch (Exception e) {
+                    return;
+                }
             }
+
             System.out.println(count);
         }
     }
