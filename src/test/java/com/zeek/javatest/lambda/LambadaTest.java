c703cc15923704be21830b64c71be4a277a8e4cf
zeeeek <liweibooooo@126.com>
2018-10-18 19:53:11 +0800

    lambada空列表测试

--- src/test/java/com/zeek/javatest/lambda/LambadaTest.java
+++ src/test/java/com/zeek/javatest/lambda/LambadaTest.java
@@ -5,11 +5,23 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Created by weibo_li on 2017/5/26.
  */
 public class LambadaTest {
+
+    @Test
+    public void testNull() {
+
+        Integer result ;
+        List< Integer> someNumbers = Arrays. asList();
+
+        List<Integer> collect = someNumbers.stream().map(integer -> integer).collect(Collectors.toList());
+        System.out.println(collect);
+
+    }
 
     @Test
     public void test() {
