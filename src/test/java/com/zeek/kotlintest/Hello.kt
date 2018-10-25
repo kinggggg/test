189cc8b36f1fc330d4da54898157da8ff7d3ef73
zeeeek <liweibooooo@126.com>
2018-10-25 20:00:08 +0800

    kotlin

--- src/test/java/com/zeek/kotlintest/Hello.kt
+++ src/test/java/com/zeek/kotlintest/Hello.kt
@@ -2,5 +2,11 @@ package com.zeek.kotlintest
 
 fun main(args: Array<String>) {
     println("Hello World!")
+
+    var af1 = 5.2123123123f;
+    println("af1的值为:${af1}");
+
+    print("\n")
+    print("aaaaaa")
 }
 
