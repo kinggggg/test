de26bf44d59bd84053c95a5ff0ecdc728ac20c44
zeeeek <liweibooooo@126.com>
2018-11-01 13:52:27 +0800

    常量和变量考察

--- src/test/java/com/zeek/kotlintest/Hello.kt
+++ src/test/java/com/zeek/kotlintest/Hello.kt
@@ -25,12 +25,39 @@ fun main(args: Array<String>) {
     var i : Int = 97;
     println(i.toChar()); //int可以转换为字符输出
 
-    println("========================")
+    println("========================类型转换")
     var sValue : Short = 5;
     println("sValue类型为${sValue.javaClass}")
 
     var iValeu : Int = 6;
     //sValue = iValeu; //只要在表达式中才能自动进行类型转换
+
+    var str = "abc"
+    //var num : Int = str.toIntOrNull(); //转换完成后的值有肯能为null或者整数值，因此num必须定义成Int?
+    //var num2 : Int? = str.toIntOrNull(); //没有此方法？？？？
+
+    println("========================编译器NPE检查")
+    var aStr : String? = "abc"
+    aStr = null;
+    //println(aStr.length) //不能编译通过，编辑自动推断aStr有可能为null，可能出现NPE空指针异常
+
+    println("========================安全调用")
+    var b : String? = "abc"
+    println(b?.length)
+    b = null;
+    println(b?.length);
+
+    println("========================字符串测试")
+    var str2 = "abc\n123" ;
+    println(str2);  //abc和123不在同一行
+
+    var str3 = """
+        | 1 \n
+        | 2
+    """.trimIndent()
+    println(str3) //\n字符原样输出
+
+
 
 
 
