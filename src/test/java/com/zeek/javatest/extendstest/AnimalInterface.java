1d793fe5b2dfb4952e265d91476ef8ee1d187056
zeeeek <liweibooooo@126.com>
2019-01-10 10:17:04 +0800

    java中接口只能继承接口，接口不能实现接口，接口也不能继承类

---
+++ src/test/java/com/zeek/javatest/extendstest/AnimalInterface.java
@@ -0,0 +1,8 @@
+package com.zeek.javatest.extendstest;
+
+/** java中接口只能继承接口，不能继承类
+ * @author: weibo_li
+ * @since: 2019-01-10 上午9:58
+ */
+public interface AnimalInterface extends AnimalInterface2  {
+}
