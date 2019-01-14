6f049d91ee0b9fb47a85ec3616d0fc27e9e54bb2
zeeeek <liweibooooo@126.com>
2019-01-14 16:14:01 +0800

    kotlin自定义异常

--- src/test/java/com/zeek/kotlintest/classtest/chapter9/chapter91/DivTest.kt
+++ src/test/java/com/zeek/kotlintest/classtest/chapter9/chapter91/DivTest.kt
@@ -1,7 +1,22 @@
 package com.zeek.kotlintest.classtest.chapter9.chapter91
 
 /**
- *
+ * kotlin异常捕获
  * @author: weibo_li
  * @since: 2019-01-14 上午10:38
  */
+fun main(args: Array<String>) {
+
+    try {
+        var a = Integer.parseInt(args[0]);
+        var b = Integer.parseInt(args[1]);
+
+        var c = a / b;
+        println("两个数相除的结果是：${c}")
+    }catch (e : RuntimeException) {
+        println("Exception")
+    }catch (ie : IndexOutOfBoundsException) {
+        println("IndexOutOfBoundsException")
+    }
+
+}
