273e149e27984cd65575f42c5ca82e88909a7eb1
zeek <liweibooooo@126.com>
2019-09-23 23:24:52 +0800

    删除不必要的引用

--- src/test/java/com/zeek/lambda/LambdaTest2.java
+++ src/test/java/com/zeek/lambda/LambdaTest2.java
@@ -1,13 +1,15 @@
 package com.zeek.lambda;
 
 import org.junit.Test;
-import scala.util.control.TailCalls;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.function.IntSupplier;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
