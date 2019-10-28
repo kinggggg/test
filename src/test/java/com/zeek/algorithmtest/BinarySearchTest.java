e416f18077a91fe43cb43cdb9e3e64d56f546e16
李伟博 <zeek_beijing@126.com>
2019-10-28 16:35:25 +0800

    二分查找递归方式

--- src/test/java/com/zeek/algorithmtest/BinarySearchTest.java
+++ src/test/java/com/zeek/algorithmtest/BinarySearchTest.java
@@ -12,11 +12,59 @@ public class BinarySearchTest {
     public static void main(String[] args) {
 
         int[] array = new int[]{1, 3, 5, 7, 9};
-        int index = BinarySearchTest.binarySearch(array, array.length, 7);
+        int index = BinarySearchTest.binarySearch(array, array.length, 17);
         System.out.println(index);
+
+        int index2 = BinarySearchTest.binarySearchRecursion(array, array.length, 7);
+        System.out.println(index2);
+    }
+
+    /**
+     * @Description: 二分查找递归形式
+     *
+     *
+     *
+     * search(int[] array, int start, int end, int target)
+     *
+     *
+     *
+     * @Author: liweibo 
+     * @Date: 2019/10/28 4:16 PM
+     * @Version: v1.0
+     *
+     * @Param null
+     * 
+     * @Return:
+     **/
+    public static int binarySearchRecursion(int[] array, int n, int target) {
+
+        return search(array, 0, n-1, target);
 
     }
 
+    public static int search(int[] array, int low, int high, int target) {
+
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = low + ((high - low) >> 1);
+
+        if (array[mid] == target) {
+            return mid;
+        }else if(array[mid] < target) {
+            low = mid + 1;
+            return search(array, low, high, target);
+        }else {
+            high = mid - 1;
+            return search(array, low, high, target);
+        }
+
+    }
+
+    
+
+    // 二分查找非递归形式
     public static int binarySearch(int[] array, int n, int target) {
 
         int low = 0;
@@ -30,13 +78,13 @@ public class BinarySearchTest {
             //优化为
 //            int mid = low + (high - low) / 2;
             //由于除法的性能不如通过位移来进行除，将除以2优化为位移
-            int mid = low + (high - low) >> 1;
+            int mid = low + ((high - low) >> 1);
 
             if (array[mid] == target) {
                 return mid;
-            }else if (array[mid] > target) {
-                low = mid + 1;
             }else if (array[mid] < target) {
+                low = mid + 1;
+            }else {
                 high = mid -1;
             }
         }
