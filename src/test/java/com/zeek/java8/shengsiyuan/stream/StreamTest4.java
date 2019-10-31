7ca22d850946bf968a9324128a40e159562662d0
李伟博 <zeek_beijing@126.com>
2019-10-31 14:50:45 +0800

    Collectors.toCollection方法考察

--- src/test/java/com/zeek/java8/shengsiyuan/stream/StreamTest4.java
+++ src/test/java/com/zeek/java8/shengsiyuan/stream/StreamTest4.java
@@ -1,7 +1,8 @@
 package com.zeek.java8.shengsiyuan.stream;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.security.cert.CollectionCertStoreParameters;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamTest4 {
@@ -9,6 +10,7 @@ public class StreamTest4 {
     public static void main(String[] args) {
 
         Stream<String> stream = Stream.of("hello", "world", "helloworld");
+
         //lambda形式参数
 //        String[] stringArray = stream.toArray(length -> new String[length]);
 //        Arrays.asList(stringArray).forEach(System.out::println);
@@ -21,8 +23,22 @@ public class StreamTest4 {
         // 手动传递参数
 //        List<String> list = stream.collect(() -> new ArrayList(), (theList, item) -> theList.add(item), (theList1, thelist2) -> theList1.addAll(thelist2));
         // 使用方法引用传递参数
-        List<String> list = stream.collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
-        list.forEach(System.out::println);
+//        List<String> list = stream.collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+//        list.forEach(System.out::println);
+
+        // 使用Collectors.toCollection就更加灵活，可以转换成任意的集合
+//        转换成ArrayList
+//        List<String> list = stream.collect(Collectors.toCollection(ArrayList::new));
+//        list.forEach(System.out::println);
+//        转换成LinkedList
+//        LinkedList<String> linkedList = stream.collect(Collectors.toCollection(LinkedList::new));
+//        linkedList.forEach(System.out::println);
+//        转换成HashSet
+//        Collection<String> hashSet = stream.collect(Collectors.toCollection(HashSet::new));
+//        hashSet.forEach(System.out::println);
+
+        String joining = stream.collect(Collectors.joining());
+        System.out.println(joining);
 
 
     }
