0da3ccbc0f326c79d7a0edbf79b339c6d84c7bf2
liweibo03 <liweibo03@kuaishou.com>
2020-09-01 15:55:15 +0800

    kotlin测试

--- src/test/java/com/zeek/kotlintest/geekvideo/Main.java
+++ src/test/java/com/zeek/kotlintest/geekvideo/Main.java
@@ -8,5 +8,25 @@ public class Main {
 
     public static void main(String[] args) {
         UtilsKt.echo("hello");
+
+        // java break也是只break内层循环
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.println("i = " + i + ", j = " + j);
+                if (j == 1) {
+                    break ;
+                }
+            }
+        }
+
+        // 带标号的break可以break外层循环
+        breakTag: for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.println("i = " + i + ", j = " + j);
+                if (j == 1) {
+                    break breakTag;
+                }
+            }
+        }
     }
 }
--- src/test/java/com/zeek/kotlintest/geekvideo/Test.kt
+++ src/test/java/com/zeek/kotlintest/geekvideo/Test.kt
@@ -23,6 +23,51 @@ fun main(args: Array<String>) {
 
     testClass(JavaMain::class.java)
     testClass(KotlinMain::class)
+
+    var a = 10;
+    println(a.unaryMinus())
+
+    var str1 = java.lang.String("hello")
+    var str2 = java.lang.String("hello")
+    println(str1 == str2)
+    var b: String? = null
+    var c: String? = null
+    println(str1?.equals(b))
+    println(null == null)
+    println(null === null)
+
+    var closeRange = 1 .. 10;
+    for (num in closeRange) {
+        println(num)
+    }
+    println(closeRange)
+    println(closeRange.count())
+    println("==============================================")
+    for (index in 0 until closeRange.count()) {
+        println(index)
+    }
+
+    println("==============================================")
+    // kotlin的break只break内层的循环
+    for (i in 0 .. 10) {
+        for (j in 0..10) {
+            println("i = $i, j = $j")
+            if (j == 1) {
+                println("break")
+                break;
+            }
+        }
+    }
+    println("==============================================")
+    for (i in 0 .. 10) {
+        for (j in 0..10) {
+            println("i = $i, j = $j")
+            if (j == 1) {
+                println("return")
+                return
+            }
+        }
+    }
 }
 
 fun printLen(str: String): String{
