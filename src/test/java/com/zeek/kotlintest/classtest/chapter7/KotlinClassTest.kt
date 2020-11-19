85f36af20c6671a0f868f9629aeb76735a54ce6a
liweibo03 <liweibo03@kuaishou.com>
2020-11-19 20:38:27 +0800

    Kotlin类

---
+++ src/test/java/com/zeek/kotlintest/classtest/chapter7/KotlinClassTest.kt
@@ -0,0 +1,25 @@
+package com.zeek.kotlintest.classtest.chapter7
+
+/**
+ * @author liweibo03 <liweibo03@kuaishou.com>
+ * Created on 2020-11-19
+ */
+class KotlinClassTest {
+
+    /**
+     *
+     * [修饰符] class 类名 [constructor 主构造器] {
+     *      0到多个次构造器定义...
+     *      0到多个属性
+     *      0到多个方法
+     * }
+     *
+     * 修饰符 public | internal | private 其中之一; final | open | abstract 其中之一
+     * open是final的反义词 修饰类, 方法和属性 表明 类可以被派生子类, 类和方法可以被重写
+     *
+     *
+     */
+
+    // Kotlin中的类型由一个或者多个有意义的单词构成, 每个单词的首字母大写, 其余字母小写
+    // 单词之间不能有任何分隔符
+}
