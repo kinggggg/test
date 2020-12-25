50e07b47b065dc0f54841b1dd1799170607a5596
kiggggg <liweiboooooo@126.com>
2020-12-25 12:15:59 +0800

    stream anyMatch

--- src/test/java/com/zeek/guava/GuavaTest.java
+++ src/test/java/com/zeek/guava/GuavaTest.java
@@ -1,10 +1,12 @@
 package com.zeek.guava;
 
+import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import org.junit.Test;
 
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
@@ -15,11 +17,15 @@ import java.util.concurrent.TimeUnit;
 public class GuavaTest {
 
     private final LoadingCache<String, String> loadingCache1 = CacheBuilder
-            .newBuilder().maximumSize(1000).expireAfterAccess(60, TimeUnit.SECONDS)
+            .newBuilder()
+            .maximumSize(1000)
+            .expireAfterAccess(5, TimeUnit.SECONDS)
             .build(new CacheLoader<String, String>() {
                 @Override
                 public String load(String key) {
-                    return "world1";
+                    String value = "world: " + new Random().nextInt();
+                    System.out.println("当前值: " + value);
+                    return value;
                 }
             });
 
@@ -38,7 +44,24 @@ public class GuavaTest {
         String hello1 = loadingCache1.get("hello");
         System.out.println(hello1);
 
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        hello1 = loadingCache1.get("hello");
+        System.out.println(hello1);
+
         String hello2 = loadingCache2.get("hello");
         System.out.println(hello2);
+
+        for (int i = 0; i < 100000; i++) {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
