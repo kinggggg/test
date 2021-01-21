0b6f9f0a1be300f1d76bb1c7ec38e13f8fdb1306
kinggggg <liweibooooo@126.com>
2021-01-21 20:43:35 +0800

    java8 lambda表达式Collectos.toMap, Collectors.groupingBy测试

--- src/test/java/com/zeek/java8/shengsiyuan/stream/StreamTest.java
+++ src/test/java/com/zeek/java8/shengsiyuan/stream/StreamTest.java
@@ -1,7 +1,10 @@
 package com.zeek.java8.shengsiyuan.stream;
 
-import java.util.Arrays;
-import java.util.List;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -14,8 +17,113 @@ import java.util.stream.Stream;
  **/
 public class StreamTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void test5() {
+        List<Integer> strings1 = Arrays.asList("ab", "bc", "dc").stream().map(ele -> ele.length()).collect(Collectors.toList());
+        strings1.forEach(System.out::println);
 
+        System.out.println("=======");
+
+        List<String> strings2 = Arrays.asList("ab", "abc", "bcd");
+        // 第三个参数是覆盖规则: 当转换为map的时候如果存在相同的key话, key对应的value应该用哪个? 其中oldValue是先根据这个key获取的值, newValue是后根据这个key获取的值
+        Map<Integer, String> toMap = strings2.stream().collect(Collectors.toMap(e -> e.length(), ele -> ele, (oldVal, newVal) -> oldVal));
+        System.out.println(toMap);
+
+        System.out.println("=======");
+
+        List<String> strings3 = Arrays.asList("ab", "bc", "bc");
+        // 按照元素本身(Function.identity())先分组, 然后将分组后每组的结果转换成List
+        Map<String, List<String>> groupByToList = strings3.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.toList()));
+        System.out.println(groupByToList);
+
+        System.out.println("=======");
+
+        List<String> strings4 = Arrays.asList("ab", "bc", "bc");
+        // 按照元素本身(Function.identity())先分组, 然后将分组后的每组的元素进行比较获取最小的
+        Map<String, Optional<String>> groupByMinBy = strings4.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.minBy((one, two) -> one.compareTo(two))));
+        System.out.println(groupByMinBy);
+
+
+    }
+
+    @Test
+    public void test4() {
+        Stream<String> stream = Stream.generate(UUID.randomUUID()::toString);
+//        stream.findFirst().ifPresent(System.out::println);
+
+//        Stream.iterate(1, i -> i + 2).limit(6).forEach(System.out::println);
+//
+//        System.out.println("=============");
+//
+//        Stream.iterate(1, i -> i + 2)
+//                .limit(6).filter(i -> i > 2)
+//                .map(i -> i * 2)
+//                .skip(4)
+//                .reduce((one, two) -> one + two)
+//                .ifPresent(System.out::println);
+//
+//        IntSummaryStatistics intSummaryStatistics = Stream.iterate(1, i -> i + 2)
+//                .filter(i -> i > 2)
+//                .mapToInt(item -> item * 2)
+//                .summaryStatistics();
+//        System.out.println(intSummaryStatistics.getMin());
+//        System.out.println(intSummaryStatistics.getMax());
+//        System.out.println(intSummaryStatistics.getAverage());
+//        System.out.println(intSummaryStatistics.getCount());
+
+        // 流类似打开文件一样，不能重复操作
+//        Stream<Integer> stream = Stream.of(1, 3, 5);
+//        System.out.println(stream.filter(i -> i > 2));
+//        System.out.println(stream.findFirst());
+    }
+
+    @Test
+    public void test3() {
+        List<String> list = Arrays.asList("hello", "world", "helloworld");
+//        list.stream().map(item -> item.toUpperCase()).forEach(System.out::println);
+//        list.stream().map(String::toUpperCase).forEach(System.out::println);
+
+        Stream<List<Integer>> stream = Stream.of(Arrays.asList(1), Arrays.asList(2, 3), Arrays.asList(4, 5, 6));
+
+        stream.flatMap(theList -> theList.stream()).map(item -> item * item).forEach(System.out::println);
+    }
+
+    @Test
+    public void test2() {
+        Stream<String> stream = Stream.of("hello", "world", "helloworld");
+
+        //lambda形式参数
+//        String[] stringArray = stream.toArray(length -> new String[length]);
+//        Arrays.asList(stringArray).forEach(System.out::println);
+        // 方法引用形式参数
+//        String[] stringArray = stream.toArray(String[]::new);
+//        Arrays.asList(stringArray).forEach(System.out::println);
+
+        // 使用JDK提供的方法
+//        List<String> list = stream.collect(Collectors.toList());
+        // 手动传递参数
+//        List<String> list = stream.collect(() -> new ArrayList(), (theList, item) -> theList.add(item), (theList1, thelist2) -> theList1.addAll(thelist2));
+        // 使用方法引用传递参数
+//        List<String> list = stream.collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+//        list.forEach(System.out::println);
+
+        // 使用Collectors.toCollection就更加灵活，可以转换成任意的集合
+//        转换成ArrayList
+//        List<String> list = stream.collect(Collectors.toCollection(ArrayList::new));
+//        list.forEach(System.out::println);
+//        转换成LinkedList
+//        LinkedList<String> linkedList = stream.collect(Collectors.toCollection(LinkedList::new));
+//        linkedList.forEach(System.out::println);
+//        转换成HashSet
+//        Collection<String> hashSet = stream.collect(Collectors.toCollection(HashSet::new));
+//        hashSet.forEach(System.out::println);
+
+//        String joining = stream.collect(Collectors.joining());
+//        System.out.println(joining);
+    }
+
+    @Test
+    public void test1() {
         Stream<String> stream1 = Stream.of("hello", "world", "hello world");
 
         String[] myArray = new String[]{"hello", "world", "hello world"};
